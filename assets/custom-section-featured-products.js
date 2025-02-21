7f2a3f8834dd084aa20ca85884f620c95e837835
Andrii Yatsola <andrii@refactorplus.com>
2025-02-21 02:30:03 +0200

    added layout, styles and tab functionality

--- assets/custom-section-featured-products.js
+++ assets/custom-section-featured-products.js
@@ -5,10 +5,45 @@ if (!customElements.get("s-featured-products")) {
       }
 
       connectedCallback() {
-        console.log("init section");
+        this.tabs = this.querySelectorAll('.js-featured-products__tab-button');
+        this.slides = this.querySelectorAll('.js-featured-products__product-card');
+
+        this.sliderElement = this.querySelector('.js-featured-products__slider');
+
+        // this.initSlider();
+
+        this.tabs.forEach(tab => {
+          tab.addEventListener('change', this.changeTabEventHandler);
+        })
       }
 
       disconnectedCallback() {
+        this.tabs.forEach(tab => {
+          tab.removeEventListener('change', this.changeTabEventHandler);
+        })
+      }
+
+      changeTabEventHandler = (event) => {
+        const currentTab = event.target.closest('.js-featured-products__tab-button').value;
+
+        this.slides.forEach(item => {
+          const tabHandle = item.dataset.tab;
+
+          if (currentTab === tabHandle) {
+            item.classList.remove('is-hidden');
+          } else {
+            item.classList.add('is-hidden');
+          }
+        })
+      }
+
+      initSlider = () => {
+        this.slider = new Flickity(this.sliderElement, {
+          cellAlign: 'left',
+          contain: true,
+          pageDots: false,
+          prevNextButtons: false
+        });
       }
     }
   );
